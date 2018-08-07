af9b111b03e9373d123e69ca8012f8c29d453d79
Tamas_Kalaszi <tamas_kalaszi@epam.com>
2018-08-07 14:15:04 +0200

    changed id to auto increment value and added link to submit a new request after another

--- src/main/java/com/epam/officematters/repository/impl/RequestRepositoryImpl.java
+++ src/main/java/com/epam/officematters/repository/impl/RequestRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.epam.officematters.repository.impl;
 
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -19,10 +18,8 @@ public class RequestRepositoryImpl implements RequestRepository {
 	@Override
 	public void save(Request request) {
 		
-        final String sql = "INSERT INTO requests (id, name, email, subject, description) VALUES (?, ?, ?, ?, ?)";
-        final String id = UUID.randomUUID().toString();
-		jdbcTemplate.update(sql, id, request.getFullName(), request.getEmailAddress(), request.getSubject(), request.getDescription());
-		System.out.println(request.getFullName());
+        final String sql = "INSERT INTO requests (name, email, subject, description) VALUES ( ?, ?, ?, ?)";
+		jdbcTemplate.update(sql, request.getFullName(), request.getEmailAddress(), request.getSubject(), request.getDescription());
 	}
 
 }
